0116fa6a958bd7c5b83f1844f90734211db180bd
Laytep <nazar.mishchenia@gmail.com>
2024-02-28 17:54:59 +0100

    implement swipe contols

--- src/GameKeyboardListener.js
+++ src/GameKeyboardListener.js
@@ -16,6 +16,15 @@ export class GameKeyboardListener {
     this.events[event].push(callback);
   }
 
+  emit(event, data) {
+    let callbacks = this.events[event];
+    if (callbacks) {
+      callbacks.forEach((callback) => {
+        callback(data);
+      });
+    }
+  }
+
   listen() {
     const map = {
       ArrowUp: 0, // Up
@@ -48,6 +57,63 @@ export class GameKeyboardListener {
     this.bindButtonPress(".retry-button", this.restart);
     this.bindButtonPress(".restart-button", this.restart);
     this.bindButtonPress(".keep-playing-button", this.keepPlaying);
+
+    // Respond to swipe events
+    var touchStartClientX, touchStartClientY;
+    var gameContainer = document.getElementsByClassName("game-container")[0];
+
+    gameContainer.addEventListener(this.eventTouchstart, function (event) {
+      if (
+        (!window.navigator.msPointerEnabled && event.touches.length > 1) ||
+        event.targetTouches.length > 1
+      ) {
+        return; // Ignore if touching with more than 1 finger
+      }
+
+      if (window.navigator.msPointerEnabled) {
+        touchStartClientX = event.pageX;
+        touchStartClientY = event.pageY;
+      } else {
+        touchStartClientX = event.touches[0].clientX;
+        touchStartClientY = event.touches[0].clientY;
+      }
+
+      event.preventDefault();
+    });
+
+    gameContainer.addEventListener(this.eventTouchmove, function (event) {
+      event.preventDefault();
+    });
+
+    gameContainer.addEventListener(this.eventTouchend, () => {
+      if (
+        (!window.navigator.msPointerEnabled && event.touches.length > 0) ||
+        event.targetTouches.length > 0
+      ) {
+        return; // Ignore if still touching with one or more fingers
+      }
+
+      var touchEndClientX, touchEndClientY;
+
+      if (window.navigator.msPointerEnabled) {
+        touchEndClientX = event.pageX;
+        touchEndClientY = event.pageY;
+      } else {
+        touchEndClientX = event.changedTouches[0].clientX;
+        touchEndClientY = event.changedTouches[0].clientY;
+      }
+
+      var dx = touchEndClientX - touchStartClientX;
+      var absDx = Math.abs(dx);
+
+      var dy = touchEndClientY - touchStartClientY;
+      var absDy = Math.abs(dy);
+
+      if (Math.max(absDx, absDy) > 10) {
+        // (right : left) : (down : up)
+        this.emit("move", absDx > absDy ? (dx > 0 ? 1 : 3) : dy > 0 ? 2 : 0);
+      }
+    });
   }
 
   restart(event) {
@@ -58,15 +124,6 @@ export class GameKeyboardListener {
   keepPlaying(event) {
     event.preventDefault();
     this.emit("keepPlaying");
-  }
-
-  emit(event, data) {
-    let callbacks = this.events[event];
-    if (callbacks) {
-      callbacks.forEach((callback) => {
-        callback(data);
-      });
-    }
   }
 
   bindButtonPress(selector, fn) {
